228c828069defb7f36b40ef74f50a4719e1a7c70
jaimin1224 <marfatiajaimin@gmail.com>
2020-04-30 12:52:59 -0400

    - Added Console Application.

---
+++ Transport.ly_App/Transport.ly.CommandLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Transport.ly.CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
